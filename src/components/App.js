ca17f2739890273f63524cc8bef6d32c1273f492
joynalam67598 <59138477+joynalam67598@users.noreply.github.com>
2021-09-07 11:13:59 +0600

    implement public and private route

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,26 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from './pages/Result';
 import Signup from "./pages/Signup";
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from "./PublicRoute";
 
 function App() {
 	return (
-		<Router>
-			<AuthProvider>
-				<Layout>
-					<Switch>
-						<Route exact path={"/"} component={Home}/>
-						<Route exact path={"/signup"} component={Signup}/>
-						<Route exact path={"/login"} component={Login}/>
-						<Route exact path={"/quiz"} component={Quiz}/>
-						<Route exact path={"/result"} component={Result}/>
-						<Route component={Error}/>
-					</Switch>
-				</Layout>
-			</AuthProvider>
-		</Router>
-	);
+    <Router>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <PublicRoute exact path={"/signup"} component={Signup} />
+            <PublicRoute exact path={"/login"} component={Login} />
+            <PrivateRoute exact path={"/quiz"} component={Quiz} />
+            <PrivateRoute exact path={"/result"} component={Result} />
+            <Route component={Error} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
