1148679ec971704233efae4502cbec98681fd007
joynalam67598 <59138477+joynalam67598@users.noreply.github.com>
2021-09-07 15:45:32 +0600

    add database authentication, rule, infinitescroll package for videos

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 
 //firebase configurations
 //Initialize Firebase
-const app = initializeApp ({
-	apiKey: process.env.React_APP_API_KEY,
-	authDomain: process.env.React_APP_AUTH_DOMAIN,
-	projectId: process.env.React_APP_PROJECT_ID,
-	storageBucket: process.env.React_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.React_APP_MESSAGING_SENDER_ID,
-	appId: process.env.React_APP_ID,
+const app = initializeApp({
+  apiKey: process.env.React_APP_API_KEY,
+  authDomain: process.env.React_APP_AUTH_DOMAIN,
+  projectId: process.env.React_APP_PROJECT_ID,
+  storageBucket: process.env.React_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.React_APP_MESSAGING_SENDER_ID,
+  appId: process.env.React_APP_ID,
+  databaseURL: process.env.React_APP_DATABASE_URL,
 });
 
 export default app;
